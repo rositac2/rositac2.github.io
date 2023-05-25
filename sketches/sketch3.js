9861df2cb51da74f6d5e6e5fe54369feb82739f7
rositac2 <125689874+rositac2@users.noreply.github.com>
2023-05-25 12:29:01 -0400

    Update sketch3.js

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -51,6 +51,5 @@ line(41, 341 , 0 , 340)
 line(502 , 351 , 505 , 399 ) // makes the line going down on the right
 
   
-//This line of code displays the mouse coordinates 
-text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
+
 }
